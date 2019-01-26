eb926ad094dba0792610f4aebb807667ae92a97b
Raphael Freire <raphaelmachadofreire@gmail.com>
2019-01-26 20:43:48 -0200

    ajustado o nome do caminho das fotos

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -2,29 +2,28 @@ const $root = document.querySelector("#root");
 const $cardsWrapper = createCardsWrapper();
 
 const $memoryCardC = createMemoryCard({
-    nameClass: "-front",
-    src: "img/icon-c.png",
-    alt: "Icone de um livro da linguagem C++"
-  });
+     nameClass: "-front",
+     src: "img/icon-c.png",
+     alt: "Icone de um livro da linguagem C++"
+});
 
 const $memoryCardJS = createMemoryCard({
-  nameClass: "-front",
-  src: "img/icon-JS.png",
-  alt: "Icone de um livro da linguagem JavaScript"
+     nameClass: "-front",
+     src: "img/icon-js.png",
+     alt: "Icone de um livro da linguagem JavaScript"
 });
 
 const $memoryCardJava = createMemoryCard({
-  nameClass: "-front",
-  src: "img/icon-Java.png",
-  alt: "Icone de um livro da linguagem Java"
+     nameClass: "-front",
+     src: "img/icon-java.png",
+     alt: "Icone de um livro da linguagem Java"
 });
 
 const $memoryCardWoman = createMemoryCard({
-  nameClass: "-front",
-  src: "img/icon-woman.png",
-  alt: "Icone de uma mulher codando"
+     nameClass: "-front",
+     src: "img/icon-woman.png",
+     alt: "Icone de uma mulher codando"
 });
-
 
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
